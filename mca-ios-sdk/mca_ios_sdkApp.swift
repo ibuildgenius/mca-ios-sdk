7fac569822ce3103006c58c59d40b6f2623d3464
Xheghun <ysenruf@gmail.com>
2022-11-22 12:47:37 +0100

    Initial Commit

---
+++ mca-ios-sdk/mca_ios_sdkApp.swift
@@ -0,0 +1,17 @@
+//
+//  mca_ios_sdkApp.swift
+//  mca-ios-sdk
+//
+//  Created by mycover Mobile on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct mca_ios_sdkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
